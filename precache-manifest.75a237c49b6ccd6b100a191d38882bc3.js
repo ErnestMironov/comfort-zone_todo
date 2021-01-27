66961c6fd2b6c0fe57db4a4479025c1836ed7f02
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2021-01-28 00:05:48 +0300

    Updates

---
+++ precache-manifest.75a237c49b6ccd6b100a191d38882bc3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae32520f020f60148cf3",
+    "url": "/comfort-zone_todo/css/app.b664b870.css"
+  },
+  {
+    "revision": "e5b72fee1375b0f8e555",
+    "url": "/comfort-zone_todo/css/chunk-vendors.71a233fb.css"
+  },
+  {
+    "revision": "1a7804f52e7d5dc5a30ad2b4b7c48288",
+    "url": "/comfort-zone_todo/img/arrow.1a7804f5.svg"
+  },
+  {
+    "revision": "d23ac634eb778b9b2213523da37943ef",
+    "url": "/comfort-zone_todo/img/complete.d23ac634.svg"
+  },
+  {
+    "revision": "98e788b4670eabde4d0e15f702004b91",
+    "url": "/comfort-zone_todo/img/pencil.98e788b4.svg"
+  },
+  {
+    "revision": "aab68f205eb0af04e5e99d47168ec56b",
+    "url": "/comfort-zone_todo/img/plus.aab68f20.svg"
+  },
+  {
+    "revision": "e103935a4963378466d0a42455abc2b5",
+    "url": "/comfort-zone_todo/img/times.e103935a.svg"
+  },
+  {
+    "revision": "439c2e2d1a7dfad461d0a3520f55cccd",
+    "url": "/comfort-zone_todo/index.html"
+  },
+  {
+    "revision": "ae32520f020f60148cf3",
+    "url": "/comfort-zone_todo/js/app.6619f43f.js"
+  },
+  {
+    "revision": "e5b72fee1375b0f8e555",
+    "url": "/comfort-zone_todo/js/chunk-vendors.69dd93eb.js"
+  },
+  {
+    "revision": "da300fdbbfa52380b11fb7f3ec12438a",
+    "url": "/comfort-zone_todo/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/comfort-zone_todo/robots.txt"
+  }
+]);
